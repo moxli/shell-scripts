b50f25b73b1d4633dc41c82fd10261d5cee93927
moxli <mdorner@mailbox.org>
2017-04-01 10:52:08 +0000

    add a script for basic backups

---
+++ basic-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+KEEP=2
+BACKUPS=$(find /backup/ -name "dokuwiki-*.tar.gz" | wc -l | sed 's/\ //g')
+while [ "$BACKUPS" -ge "$KEEP" ]
+do
+ls -tr1 /backup/dokuwiki-*.tar.gz | head -n 1 | xargs rm -f
+BACKUPS=$(expr "$BACKUPS" - 1)
+done
+DATE=$(date +%Y%m%d%H%M%S)
+rm -f /backup/.dokuwiki-"${DATE}".tar.gz_INPROGRESS
+cd /backup/
+tar -cvpzf .dokuwiki-"${DATE}".tar.gz_INPROGRESS /var/www/dokuwiki/
+mv -f /backup/.dokuwiki-"${DATE}".tar.gz_INPROGRESS /backup/dokuwiki-"${DATE}".tar.gz
+exit 0
