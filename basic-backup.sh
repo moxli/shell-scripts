3658ba4767d9c8df2e8f7601d7ca340a11cc9162
moxli <moxli@mailbox.org>
2018-01-12 11:45:29 +0100

    Added handling of received kernel signals
    The script will now remove the temporary file that is created
    during the backup process, in case it receives one of the following
    signals: SIGHUP SIGINT SIGQUIT SIGTERM

--- basic-backup.sh
+++ basic-backup.sh
@@ -9,16 +9,30 @@ DIRTOBACKUP="/home/mdorner/dotfiles/" # the directory you want to backup
 BACKUPDIR="/mnt/backup1.moxli.de/dotfiles/" # where the backup file will be stored
 KEEP=2 # the number of backups you want to keep
 #####
+DATE=$(date +%Y%m%d%H%M%S)
+BACKUPROOT=$(dirname $DIRTOBACKUP)
+SERVICEDIR=$(basename $DIRTOBACKUP)
+#####
+trap clean_up SIGHUP SIGINT SIGQUIT SIGTERM
+#####
+function clean_up() {
+    echo "you propably hit Ctrl-C or I received a signal(HUP/INT/QUIT/TERM)"
+    echo "removing the temporary files..."
+    rm -f "$BACKUPDIR.$SERVICE-${DATE}.tar.gz_INPROGRESS"
+    echo "successfully removed the temporary file"
+    echo "exiting now!"
+    exit 1
+}
+#####
 if [ ! -d "$BACKUPDIR" ]
 then
     echo "The directory to backup to($BACKUPDIR) does not exist."
     echo "Creating $BACKUPDIR..."
-    if [ ! "$(mkdir -p $BACKUPDIR)" ]
+    if [ "$(mkdir -p $BACKUPDIR)" ]
     then
         exit 1
     else
         echo "Created $BACKUPDIR successfully."
-
     fi
 fi
 BACKUPS=$(find "$BACKUPDIR" -name "$SERVICE-*.tar.gz" | wc -l | sed 's/\ //g')
@@ -26,11 +40,8 @@ while [ "$BACKUPS" -ge "$KEEP" ]; do
     ls -tr1 "$BACKUPDIR$SERVICE"-*.tar.gz | head -n 1 | xargs rm -f
     BACKUPS=$(("$BACKUPS" -1))
 done
-DATE=$(date +%Y%m%d%H%M%S)
 rm -f "$BACKUPDIR.$SERVICE-${DATE}.tar.gz_INPROGRESS"
 cd "$BACKUPDIR" || exit 1
-BACKUPROOT=$(dirname $DIRTOBACKUP)
-SERVICEDIR=$(basename $DIRTOBACKUP)
 tar -cpzf ".$SERVICE-${DATE}.tar.gz_INPROGRESS" -C "$BACKUPROOT" "$SERVICEDIR"
 mv -f "$BACKUPDIR.$SERVICE-${DATE}.tar.gz_INPROGRESS" "$BACKUPDIR$SERVICE-${DATE}.tar.gz"
 exit 0
