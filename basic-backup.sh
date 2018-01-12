ca076859721b9658cd8ae36061f0068aeab15e45
moxli <moxli@mailbox.org>
2018-01-12 10:35:19 +0100

    Made script easier to use and more scaleable
    You just need to change four variables according to your needs
    and you are good to go. I also hardened the script a little bit,
    more (incl. SIGNAL handling) will follow. I also fixed all problems
    and recommendation from shellcheck(I am still looking for a fix for
    SC2012).

--- basic-backup.sh
+++ basic-backup.sh
@@ -1,14 +1,37 @@
-#!/bin/bash
-KEEP=2
-BACKUPS=$(find /backup/ -name "dokuwiki-*.tar.gz" | wc -l | sed 's/\ //g')
-while [ "$BACKUPS" -ge "$KEEP" ]
-do
-ls -tr1 /backup/dokuwiki-*.tar.gz | head -n 1 | xargs rm -f
-BACKUPS=$(expr "$BACKUPS" - 1)
+#!/usr/bin/env bash
+#####
+# Purpose: Backup a specific directory and delete old backups
+#####
+# Please change the four variable below according to your needs
+#####
+SERVICE="dotfiles" # what the finished backup fill will be called
+DIRTOBACKUP="/home/mdorner/dotfiles/" # the directory you want to backup
+BACKUPDIR="/mnt/backup1.moxli.de/dokuwiki/" # where the backup file will be stored
+KEEP=2 # the number of backups you want to keep
+#####
+if [ ! -d "$BACKUPDIR" ]
+then
+    echo "The directory to backup to($BACKUPDIR) does not exist."
+    echo "Creating $BACKUPDIR..."
+    if [ ! "$(mkdir -p $BACKUPDIR)" ]
+    then
+        echo "Could not create $BACKUPDIR, failed with status $?."
+        exit 0
+    else
+        echo "Created $BACKUPDIR successfully."
+
+    fi
+fi
+BACKUPS=$(find "$BACKUPDIR" -name "$SERVICE-*.tar.gz" | wc -l | sed 's/\ //g')
+while [ "$BACKUPS" -ge "$KEEP" ]; do
+    ls -tr1 "$BACKUPDIR$SERVICE"-*.tar.gz | head -n 1 | xargs rm -f
+    BACKUPS=$(("$BACKUPS" -1))
 done
 DATE=$(date +%Y%m%d%H%M%S)
-rm -f /backup/.dokuwiki-"${DATE}".tar.gz_INPROGRESS
-cd /backup/ || exit
-tar -cvpzf .dokuwiki-"${DATE}".tar.gz_INPROGRESS /var/www/dokuwiki/
-mv -f /backup/.dokuwiki-"${DATE}".tar.gz_INPROGRESS /backup/dokuwiki-"${DATE}".tar.gz
+rm -f "$BACKUPDIR.$SERVICE-${DATE}.tar.gz_INPROGRESS"
+cd "$BACKUPDIR" || exit 1
+BACKUPROOT=$(dirname $DIRTOBACKUP)
+SERVICEDIR=$(basename $DIRTOBACKUP)
+tar -cpzf ".$SERVICE-${DATE}.tar.gz_INPROGRESS" -C "$BACKUPROOT" "$SERVICEDIR"
+mv -f "$BACKUPDIR.$SERVICE-${DATE}.tar.gz_INPROGRESS" "$BACKUPDIR$SERVICE-${DATE}.tar.gz"
 exit 0
